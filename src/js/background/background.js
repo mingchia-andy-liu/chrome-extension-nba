e152e4fa910f9c03fe155dc5bc012124adb9f642
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2017-11-05 01:21:41 -0700

    🗑 Remove unneeded code

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -64,26 +64,20 @@ const liveAlarmId = setInterval(liveCallBack, 30 * 60 * 1000)
 function validateLiveGame(match) {
     if (match.stt === 'Final') {
         //finish
-        match._status = 'finish'
         return 'finish'
     } else if (match && !match.cl) {
         // haven't started
-        match._status = 'prepare'
         return 'prepare'
     } else if (match.stt === 'Halftime' || match.stt.includes('End') || match.stt.includes('Start')) {
         // live
-        match._status = 'live'
         return 'live'
     } else if (match.cl === '00:00.0') {
         if (match.stt.includes('ET') || match.stt.includes('pm') || match.stt.includes('am') || match.stt === 'PPD') {
-            match._status = 'prepare'
             return 'prepare'
         }
     } else if (match.cl !== '' && match.cl !== '00:00.0') {
-        match._status = 'live'
         return 'live'
     }
-    match._status = 'prepare'
     return 'prepare'
 }
 
