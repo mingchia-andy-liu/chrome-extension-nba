6b7b5860a2f98014d7bf13bc4e9ad8e6c6baa924
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2017-12-27 19:53:23 -0800

    ⏰ Notification at 12 every day
    - Move alarm to a new folders
    - Set up alarms for the day of every day at 12
      - When the alarm fires, compares the scheduled time
      - to check the game
    - Require more JS into background
    
    TODO: change the check, checking the time seems bad

--- src/js/common/games.js
+++ src/js/common/games.js
@@ -23,3 +23,66 @@ function validateLiveGame(match) {
     match._status = 'prepare'
     return 'prepare'
 }
+
+/**
+ * Check if there is any live games going one right now
+ * @param {array} games
+ * @returns {bool} true if there is one or more live game, false otherwise
+ */
+function anyLiveGames(games) {
+    return !!games.find(function(match){
+        return match._status === 'live'
+    })
+}
+
+function preprocessData(games) {
+    // preprocess the data before anything
+    var live = []
+    var finish = []
+    var prepare = []
+    games.forEach(function(game, index) {
+        // adding property so the schedule in DATE_UTILS can be found
+        game.gdte = moment(game.gcode.split('/')[0]).format('YYYY-MM-DD')
+        switch (validateLiveGame(game)) {
+            case 'prepare':
+                game._localTime = getGameStartTime(game.stt, game.gcode)
+                if (prepare.length === 0) {
+                    prepare.push(game);
+                    break
+                }
+                const start = moment(game._localTime, ["h:mm A"])
+                for(let i = 0; i < prepare.length; i++) {
+                    // prepare items should have localTime because they were just inserted
+                    const end = moment(prepare[i]._localTime, ["h:mm A"])
+                    if (start.isBefore(end)) {
+                        prepare.splice(i, 0, game)
+                        break
+                    } else if (i === prepare.length - 1) {
+                        // latest
+                        prepare.push(game)
+                        break
+                    }
+                }
+                break;
+            case 'live':
+                live.push(game);
+                break;
+            case 'finish':
+                finish.push(game)
+                break;
+            default:
+                finish.push(game)
+        }
+    })
+    return live.concat(finish.concat(prepare))
+}
+
+function getGameStartTime(status, gcode) {
+    var date = gcode.split('/')[0]
+    var today = moment(date, ["YYYYMMDD"]).format("YYYY-MM-DD")
+    var gameTime = moment(status, ["h:mm A"]).format("HH:mm");
+    var zone = "America/New_York";
+    var input = `${today} ${gameTime}`
+    var result = moment.tz(input, zone).local().format("hh:mm A");
+    return result
+}
