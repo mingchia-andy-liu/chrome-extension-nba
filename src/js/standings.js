c003fd0885ea6c14a2b4194eaa571a4a6953c4b9
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2018-02-13 16:12:23 -0800

    ⚒ DRY code

--- src/js/standings.js
+++ src/js/standings.js
@@ -5,23 +5,19 @@ $(function(){
         url: 'https://stats.nba.com/stats/leaguestandingsv3?LeagueID=00&Season=2017-18&SeasonType=Regular+Season'
     }).done(function(data){
         const resultSet = data.resultSets[0]
-        const headers = resultSet.headers
-        /** 4: TeamName 5: Conference 12: WINS 13: LOSSES 14: WinPCT 16: Record 19: L10 22: OT 56: PointsPG 57: OppPointsPG 58: DiffPointsPG
-        */
+        // const headers = resultSet.headers
         const teams = resultSet.rowSet
         const east = teams.filter(function(team){
             return team[5] === 'East'
         })
         const west = teams.filter(function(team){
-            return team[5] != 'East'
+            return team[5] !== 'East'
         })
         const eastern = $('#eastern')[0]
         const western = $('#western')[0]
+        $(eastern.rows[8]).find('td').addClass('u-8th-seed')
+        $(western.rows[8]).find('td').addClass('u-8th-seed')
         for (let i = 1; i <= 15;i++) {
-            if (i === 8) {
-                $(eastern.rows[i]).find('td').addClass('u-8th-seed')
-                $(western.rows[i]).find('td').addClass('u-8th-seed')
-            }
             $(eastern.rows[i].cells[0]).text(east[i-1][4])
             $(eastern.rows[i].cells[1]).text(east[i-1][12])
             $(eastern.rows[i].cells[2]).text(east[i-1][13])
@@ -90,8 +86,6 @@ $(function(){
         $('#western tr > td:nth-child(11)').each(findMinMax)
         $(`#western tr:nth-child(${maxIndex+2}) > td:nth-child(11)`).css('color','#00bb00')
         $(`#western tr:nth-child(${minIndex+2}) > td:nth-child(11)`).css('color','red')
-        resetMinMax()
-
     }).fail(function(xhr, textStatus, errorThrown) {
         console.log('Failed to fetch data.');
     });
