aeec945864f452b2834a6c9e59e4d4b895b5c4cc
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2018-03-18 16:25:42 -0700

    📦 Update to version 1.2.2
    - Add play by play to each game
    - Update styles for smaller screen sizes
    - Change the calender library
      - Remove flatpickr and use jQuery UI
    - Load font files locally
    - Update GA function w/o injecting script tag
    - Fix minimum date issue
    - Fix time styles issue
    - Add the distributed binary files

--- src/js/ga.js
+++ src/js/ga.js
@@ -1,24 +1,20 @@
-// /* Chrome
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-114000944-1', 'auto');
 ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check @see https://stackoverflow.com/a/22152353
 ga('require', 'displayfeatures');
 ga('send', 'pageview',location.pathname);
-// */
 
 
-// /* FF
 function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
-  }
+}
 
-const uuid = s4() + '.' + s4()
+window.uuid = window.uuid || s4() + '.' + s4()
 $.ajax({
     type: 'POST',
-    url: 'https://www.google-analytics.com/collect?v=1&cid=' + uuid + '&tid=UA-114000944-2&t=pageview&dp=' + location.pathname,
+    url: 'https://www.google-analytics.com/collect?v=1&cid=' + uuid + '&tid=UA-114000944-1&t=pageview&dp=' + location.pathname,
 }).fail(function(xhr, textStatus, errorThrown) {
     console.log('Failed to send report to ga');
 });
-// */
