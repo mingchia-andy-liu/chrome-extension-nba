b3fe425c8a357871dbd730ad51cadd22fd6d2456
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2018-04-04 10:59:14 +1000

    Remove firefox ga

--- src/js/ga.js
+++ src/js/ga.js
@@ -3,18 +3,3 @@ ga('create', 'UA-114000944-1', 'auto');
 ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check @see https://stackoverflow.com/a/22152353
 ga('require', 'displayfeatures');
 ga('send', 'pageview',location.pathname);
-
-
-function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-}
-
-window.uuid = window.uuid || s4() + '.' + s4()
-$.ajax({
-    type: 'POST',
-    url: 'https://www.google-analytics.com/collect?v=1&cid=' + uuid + '&tid=UA-114000944-1&t=pageview&dp=' + location.pathname,
-}).fail(function(xhr, textStatus, errorThrown) {
-    console.log('Failed to send report to ga');
-});
