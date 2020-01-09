effc7ef4b037219236431c8b09797dcf128b58c9
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2020-01-08 23:06:20 -0800

    gh-129 (fix): temp

--- src/app/containers/Popup/actions.js
+++ src/app/containers/Popup/actions.js
@@ -37,9 +37,12 @@ const fetchGames = async (dispatch, dateStr, callback, isBackground) => {
 
 export const fetchRequest = async (dateStr) => {
     try {
-        const res = await fetch(`https://data.nba.com/data/5s/json/cms/noseason/scoreboard/${dateStr}/games.json`)
-        const { sports_content: { games: { game } } } = await res.json()
-        return game
+        // const res = await fetch(`https://data.nba.com/data/5s/json/cms/noseason/scoreboard/${dateStr}/games.json`)
+        // const res = await fetch(`http://data.nba.net/prod/v2/${dateStr}/scoreboard.json`)
+        const res = await fetch(`https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2019/scores/00_todays_scores.json`, {referrer: "https://cross.origin/page.html"})
+        const { gs : {g : games} } = await res.json()
+        console.log(games);
+        return games;
     } catch (error) {
         return []
     }
--- src/app/containers/Popup/reducers.js
+++ src/app/containers/Popup/reducers.js
@@ -7,35 +7,68 @@ const initState = {
     lastUpdate: new Date(0),
 }
 
+
 const getBroadcasters = (casters) => {
-    if (casters && casters.tv && casters.tv.broadcaster) {
-        return casters.tv.broadcaster
-    } else {
-        return []
-    }
+    const national = (casters && casters.national)
+    const canadian = (casters && casters.canadian)
+    const home = (casters && casters.home)
+    const visitor = (casters && casters.visitor)
+
+    return national.concat(canadian).concat(home).concat(visitor)
 }
 
+// for http://data.nba.net/prod/v2/${dateStr}/scoreboard.json
+// const sanitizeGame = game => ({
+//     id: game.gameId,
+//     // date: game.date,
+//     date: game.startDateEastern,
+//     // time: game.time,
+//     time: game.startTimeEastern,
+//     state: game.state,
+//     arena: {
+//         name: game.arena.name,
+//         city: game.arena.city,
+//     },
+//     broadcasters: getBroadcasters(game.watch.broadcast.broadcasters),
+//     home: game.hTeam,
+//     visitor: game.vTeam,
+//     playoffs: game.playoffs || {},
+//     periodTime: {
+//         // have not start
+//         periodStatus: game.period.current === 1
+//             ? moment.tz(`${game.date}${game.time}`, 'YYYYMMDDhhmm', 'America/New_York').local().format('hh:mm A')
+//             : game.period.current,
+//         gameClock: game.clock,
+//         gameStatus: game.statusNum,
+//         periodValue: game.period.current,
+//     },
+// })
+
+
+// https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/2019/scores/00_todays_scores.json
 const sanitizeGame = game => ({
-    id: game.id,
-    date: game.date,
-    time: game.time,
+    id: game.gid,
+    // date: game.date,
+    date: game.lm.gdate,
+    // time: game.time,
+    time: game.startTimeEastern,
     state: game.state,
     arena: {
-        name: game.arena,
-        city: game.city,
+        name: game.arena.name,
+        city: game.arena.city,
     },
-    broadcasters: getBroadcasters(game.broadcasters),
-    home: game.home,
-    visitor: game.visitor,
-    playoffs: game.playoffs,
+    broadcasters: getBroadcasters(game.watch.broadcast.broadcasters),
+    home: game.hTeam,
+    visitor: game.vTeam,
+    playoffs: game.playoffs || {},
     periodTime: {
         // have not start
-        periodStatus: game.period_time.game_status === '1'
-            ? moment.tz(`${game.date}${game.time}`, 'YYYYMMDDhhmm', 'America/New_York').local().format('hh:mm A')
-            : game.period_time.period_status,
-        gameClock: game.period_time.game_clock,
-        gameStatus: game.period_time.game_status,
-        periodValue: game.period_time.period_value,
+        periodStatus: game.p === 1
+            ? moment.tz(`${game.lm}${game.time}`, 'YYYY-MM-DDhhmm', 'America/New_York').local().format('hh:mm A')
+            : game.period.current,
+        gameClock: game.cl,
+        gameStatus: game.stt,
+        periodValue: game.p,
     },
 })
 
@@ -45,6 +78,8 @@ const sanitizeGame = game => ({
  */
 const sanitizeGames = games => {
     const sanitized = games.map(game => sanitizeGame(game))
+    // eslint-disable-next-line no-console
+    console.log(sanitized)
     const prepare = sanitized.filter(game => game && game.periodTime && game.periodTime.gameStatus === '1')
     const live = sanitized.filter(game => game && game.periodTime && game.periodTime.gameStatus === '2')
     const finish = sanitized.filter(game => game && game.periodTime && game.periodTime.gameStatus === '3')
