ac82154450b624b7b928aaa5aefa761c8386a41f
Andy Liu <mingchia.andy.liu@gmail.com>
2020-07-28 18:41:36 -0700

    [#190] (fix): bg error handler (#191)
    * properly handle error in background script for live game check

--- src/app/background.js
+++ src/app/background.js
@@ -48,16 +48,25 @@ const liveListener = () => {
         })
         .catch(() => {
             return fetch(`http://data.nba.net/prod/v2/${dateStr}/scoreboard.json`)
+                .then(res => res.json())
+                .then(({games}) => checkLiveGame(games, 2))
+                .catch(() => {
+                    const date = moment(dateStr)
+                    let year
+                    if (date.year() === 2020) {
+                        // 2020 season is delayed and season should finish in 2020-09
+                        year = date.month() > 8 ? date.year() : date.add(-1, 'years').year()
+                    } else {
+                        // if it's after july, it's a new season
+                        year = date.month() > 5  ? date.year() : date.add(-1, 'years').year()
+                    }
+                    return fetch(`https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/${year}/scores/00_todays_scores.json`)
+                        .then(res => res.json())
+                        .then(({gs: {g}})=> {
+                            checkLiveGame(g, 1)
+                        })
+                })
         })
-        .then(res => res.json())
-        .then(({games}) => checkLiveGame(games, 2))
-        .catch(() => {
-            const date = moment(dateStr)
-            const year = date.month() > 5 ? date.year() : date.add(-1, 'years').year()
-            return fetch(`https://data.nba.com/data/5s/v2015/json/mobile_teams/nba/${year}/scores/00_todays_scores.json`)
-        })
-        .then(res => res.json())
-        .then(({gs: {g}})=> checkLiveGame(g, 1))
         .catch(() => browser.setBadgeText({ text: '' }))
 }
 
