2aecaca2d50b2486f2e53a1d02205f806b9f9dfe
Andy Liu <mingchia.andy.liu@gmail.com>
2025-01-09 19:43:11 -0800

    unused

--- src/app/components/TeamInfo.jsx
+++ src/app/components/TeamInfo.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { getLogoColorByName, TEAM_ID } from '../utils/teams'
 import { SettingsConsumer } from './Context'
 import TeamLogo from './TeamLogo'
 
