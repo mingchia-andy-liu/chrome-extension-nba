0d0994fc853ab4e58cfec55a3fcfe506a6bc7e47
Andy Liu <mingchia.andy.liu@gmail.com>
2025-06-22 16:37:28 -0700

    Fix offseason detection

--- src/app/utils/getApiDate.js
+++ src/app/utils/getApiDate.js
@@ -43,19 +43,13 @@ export const isOffseason = (date) => {
       isAfter(d, new Date('2020-10-12')) && isBefore(d, new Date('2020-12-21'))
     )
   } else {
-    // betwen july and augest
+    // between july and august
     const month = getMonth(d)
     const day = getDate(d)
     if (isNaN(month) || isNaN(day)) {
       return false
     }
 
-    if (month === 8) {
-      return day < 20
-    }
-    if (month === 5) {
-      return day > 20
-    }
     return month < 8 && month > 6
   }
 }
