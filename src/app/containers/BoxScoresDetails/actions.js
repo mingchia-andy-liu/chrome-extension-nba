42a2e74928c7db62faded63bfcf5e0461997b504
Andy Liu <mingchia.andy.liu@gmail.com>
2026-05-11 23:23:21 -0700

    fix: bs 403 (#943)

--- src/app/containers/BoxScoresDetails/actions.js
+++ src/app/containers/BoxScoresDetails/actions.js
@@ -60,7 +60,24 @@ const fetchPBP = async (dateStr, gid) => {
       },
     }
   } catch (error) {
-    return {}
+    try {
+      const pbp = await fetch(
+        `https://proxy.boxscores.site?apiUrl=cdn.nba.com/static/json/liveData/playbyplay/playbyplay_${gid}.json`
+      )
+      const json = await pbp.json()
+      const actions = json?.game?.actions
+      const plays = actions
+      const isProxy = false
+      return {
+        isProxy,
+        game: {
+          gameId: gid,
+          play: plays,
+        },
+      }
+    } catch (err2) {
+      return {}
+    }
   }
 }
 
@@ -81,7 +98,22 @@ const fetchGameDetail = async (_, gid) => {
       game: g,
     }
   } catch (error) {
-    return {}
+    try {
+      const bs = await fetch(
+        `https://proxy.boxscores.site?apiUrl=cdn.nba.com/static/json/liveData/boxscore/boxscore_${gid}.json`
+      )
+      const json = await bs.json()
+      const game = json.game
+      const g = game
+      const isProxy = false
+
+      return {
+        isProxy,
+        game: g,
+      }
+    } catch (err2) {
+      return {}
+    }
   }
 }
 
